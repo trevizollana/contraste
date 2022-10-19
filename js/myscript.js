0e8c6ecbdf26e09a3639478e7d3589fce4b5d4fb
trevizollana <105882511+trevizollana@users.noreply.github.com>
2022-10-19 16:39:56 -0300

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,52 @@
+function contrasteBranco(){
+    var root = document.querySelector(":root");
+    root.style.setProperty('--cor_nav','white');
+    root.style.setProperty('--cor_header','white');
+    root.style.setProperty('--cor_footer','white');
+    root.style.setProperty('--cor_button','black');
+    root.style.setProperty('--cor_button_hover','black');
+    root.style.setProperty('--cor_reserva','black');
+    root.style.setProperty('--cor_reserva_hover','black');
+    root.style.setProperty('--cor_pedido','black');
+    root.style.setProperty('--cor_pedido_hover','black');
+    root.style.setProperty('--cor_main','white');
+    root.style.setProperty('--cor_fonte_reserva','white');
+    root.style.setProperty('--cor_fonte_pedido','white');
+    root.style.setProperty('--cor_cardapio','black');
+    root.style.setProperty('--cor_fonte_footer','black');
+    root.style.setProperty('--cor_fonte_main','black');
+    root.style.setProperty('--cor_fonte_h3','black');
+    root.style.setProperty('--cor_fonte_header','black');
+    root.style.setProperty('--cor_fonte_preta','black');
+    root.style.setProperty('--cor_fonte_button','white');
+    
+
+}
+
+function contrastePreto(){
+    var root = document.querySelector(":root");
+    root.style.setProperty('--cor_nav','black');
+    root.style.setProperty('--cor_header','black');
+    root.style.setProperty('--cor_footer','black');
+    root.style.setProperty('--cor_button','white');
+    root.style.setProperty('--cor_button_hover','black');
+    root.style.setProperty('--cor_reserva','white');
+    root.style.setProperty('--cor_reserva_hover','white');
+    root.style.setProperty('--cor_pedido','white');
+    root.style.setProperty('--cor_pedido_hover','white');
+    root.style.setProperty('--cor_cb','black');
+    root.style.setProperty('--cor_cp','black');
+    root.style.setProperty('--cor_main','black');
+    root.style.setProperty('--cor_fonte_cb','white');
+    root.style.setProperty('--cor_fonte_cp','white');
+    root.style.setProperty('--cor_fonte_reserva','black');
+    root.style.setProperty('--cor_fonte_pedido','black');
+    root.style.setProperty('--cor_cardapio','white');
+    root.style.setProperty('--cor_fonte_footer','white');
+    root.style.setProperty('--cor_fonte_main','white');
+    root.style.setProperty('--cor_fonte_h3','white');
+    root.style.setProperty('--cor_fonte_header','white');
+    root.style.setProperty('--cor_fonte_preta','white');
+    root.style.setProperty('--cor_fonte_button','black');
+
+}
